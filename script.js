f216c1109b52ec4d9a4f1ca3b7b96fc697f357f6
Wilbur125 <matt.s.wilbur@gmail.com>
2020-11-19 14:31:28 -0800

    added current day of the week, month, and date to display in HTML header

---
+++ script.js
@@ -0,0 +1,31 @@
+//displays current date
+const date = new Date();
+
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+const dayOfWeek = weekday[date.getDay()];
+
+const month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+const monthString = month[date.getMonth()];
+
+const day = date.getDate();
+
+document.getElementById("currentDay").innerHTML = dayOfWeek + ", " + monthString + " " + day;
