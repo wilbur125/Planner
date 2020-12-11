d773bc6437a25d4f6e2276850d4c82dea77af335
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-11 02:17:36 -0800

    needed to utilize moment.js. I dropped my calendar and time arrays and replaced with moment js. Also reverted current hour to show current time

--- script.js
+++ script.js
@@ -1,49 +1,18 @@
-//displays current date
-const date = new Date();
-
-const weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-const dayOfWeek = weekday[date.getDay()];
-
-const month = new Array(12);
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
-const monthString = month[date.getMonth()];
-
-const day = date.getDate();
-
-document.getElementById("currentDay").innerHTML = dayOfWeek + ", " + monthString + " " + day;
+document.getElementById("currentDay").innerHTML = moment().format('dddd MMM Do YYYY');
 
 $(document).ready(function() {
     $(".saveBtn").on('click', function() {
         let time  = $(this).parent().attr("id");
         let details = $(this).siblings('.description').val();
         localStorage.setItem(time, details);
-        console.log(details);
-    });
+     });
 
     function colorCode() {
-        let currentHour = 11;
-        console.log("wut",currentHour);
+        let currentHour = moment().format('hh');
+        console.log(currentHour);
+
         $(".time-block").each(function() {  
             let timeSlot = parseInt($(this).attr('id'));
-            console.log(timeSlot);
 
             if (currentHour === timeSlot) {
                 $(this).addClass("present");
@@ -69,7 +38,6 @@ $(document).ready(function() {
     $("#15 .description").val(localStorage.getItem("15"));
     $("#16 .description").val(localStorage.getItem("16"));
     $("#17 .description").val(localStorage.getItem("17"));
-
 })
 
 
